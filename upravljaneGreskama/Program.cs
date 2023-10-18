c0a32a84fe65b06cf82d1ba347f662614720767e
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-10-18 08:25:09 +0200

    Add files via upload

---
+++ upravljaneGreskama/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljaneGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool uspijeh = false;
+
+            do
+            {
+                Console.WriteLine("Upiši svoju godinu rođenja: ");
+
+                try
+                {
+
+                    godRod = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Rođen si " + godRod + ". godine.");
+
+                    uspijeh = true;
+                }
+
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+
+                    uspijeh=false;
+                }
+            }
+
+            while (uspijeh == false);
+
+            Console.ReadKey();
+        }
+
+    }
+}
